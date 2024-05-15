ef0626714573925e690cb60da5547586787091e5
raneemAj <90855260+Nem7l@users.noreply.github.com>
2024-05-15 19:28:55 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.markdown("<h1 style='text-align: center; font-weight: bold;'>My Job Posting App</h1>", unsafe_allow_html=True)
-       st.markdown(
+    st.markdown(
         """
         <style>
         body {
