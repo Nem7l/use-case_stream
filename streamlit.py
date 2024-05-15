608caac3a051eb6f76e87b6d779922ab523ae4f3
raneemAj <90855260+Nem7l@users.noreply.github.com>
2024-05-15 19:28:29 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,20 @@
 import streamlit as st
 
 def main():
-    st.title("My Job Posting App")
-    st.image('Shutterstock_1248459142.jpg', use_column_width=True) 
+    st.markdown("<h1 style='text-align: center; font-weight: bold;'>My Job Posting App</h1>", unsafe_allow_html=True)
+       st.markdown(
+        """
+        <style>
+        body {
+            background-image: url('Shutterstock_1248459142-job-search-2.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    
     st.image('Q1-1.png', use_column_width=True)
     st.image('Q1-2.png', use_column_width=True)
     st.image('Q2.png', use_column_width=True)
