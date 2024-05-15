49c1113bff7c69dcce417c229d414b04b7eb8dbf
raneemAj <90855260+Nem7l@users.noreply.github.com>
2024-05-15 19:17:52 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,16 +1,15 @@
 import streamlit as st
 
 def main():
+    st.title("My Job Posting App")
+    st.image('Q1-1.png', use_column_width=True)
+    st.image('Q1-2.png', use_column_width=True)
+    st.image('Q2.png', use_column_width=True)
+    st.image('Q3-1.png', use_column_width=True)
+    st.image('Q3-2.png', use_column_width=True)
 
-    st.title("My Jop Posting APP")
-st.image('Q1-1'.png, use_column_width=True)
-st.image('Q1-2.png', use_column_width=True)
-st.image('Q2.png', use_column_width=True)
-st.image('Q3-1.png', use_column_width=True)
-st.image('Q3-2.png', use_column_width=True)
     name = st.text_input("Enter your name", "")
-
-   
+    
     if st.button("Submit"):
         st.write(f"Hello, {name}!")
 
@@ -18,8 +17,6 @@ st.image('Q3-2.png', use_column_width=True)
     st.subheader("Data Visualization")
     data = [1, 2, 3, 4, 5]
     st.line_chart(data)
-
- 
 
 # Run the app
 if __name__ == "__main__":
