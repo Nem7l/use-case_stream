dd60430e28605c43bdb8bfb353383af219b6030b
raneemAj <90855260+Nem7l@users.noreply.github.com>
2024-05-15 20:05:10 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,25 +1,23 @@
 import streamlit as st
 
 def main():
-    st.markdown("<h1 style='text-align: center; font-weight: bold;'>My Job Posting App</h1>", unsafe_allow_html=True)
-    st.markdown(
-        """
-        <style>
-        body {
-            background-image: url('Shutterstock_1248459142-job-search-2.jpg');
-            background-repeat: no-repeat;
-            background-size: cover;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
     
+st.markdown("<h1 style='text-align: center; font-weight: bold;'>My Job Posting App</h1>", unsafe_allow_html=True)
+
+st.image('Shutterstock_1248459142-job-search-2', use_column_width=True)
+
+st.markdown("""Jobs in Saudi Arabia: Saudi Arabia offers a wide range of job opportunities across various industries and sectors, reflecting a vibrant and diverse job market""")
+    Regional 
+st.markdown("""Job Market: The picture depicts job postings in Saudi Arabia, specific to each region, indicating a diverse and segmented job market.""") 
     st.image('Q1-1.png', use_column_width=True)
     st.image('Q1-2.png', use_column_width=True)
+st.markdown("""the graph It highlights a significant change in the job market, where opportunities are presented without any gender preference""")
     st.image('Q2.png', use_column_width=True)
+
+st.markdown("""The picture illustrates the expected salary for recent graduates which it between 3000-12000 and most of them in the range between 4000-7000""")
     st.image('Q3-1.png', use_column_width=True)
     st.image('Q3-2.png', use_column_width=True)
+
 
 
 if __name__ == "__main__":
